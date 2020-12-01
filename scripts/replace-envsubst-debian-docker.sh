0fd08dc88e63418306485a056d91312e5c314a6c
Volm, David <david.volm@wildcardcorp.com>
2020-12-01 14:48:55 -0600

    need to get USER_BASE location

--- scripts/replace-envsubst-debian-docker.sh
+++ scripts/replace-envsubst-debian-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 _ENVSUBST_LOCATION=$(which envsubst)
 rm ${_ENVSUBST_LOCATION}
-ln -s $(pyenv root)/bin/envsubst-mustache ${_ENVSUBST_LOCATION}
+ln -s $(python3 -m site | grep USER_BASE | python3 -c "import sys;sys.stdout.write(list(sys.stdin)[0].split('\'')[1])")/bin/envsubst-mustache ${_ENVSUBST_LOCATION}
